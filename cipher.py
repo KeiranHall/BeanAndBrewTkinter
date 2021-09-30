ffecd7ada30ea824bd8b0c27970588656dd1e993
KeiranHall <90696245+KeiranHall@users.noreply.github.com>
2021-09-30 14:21:07 +0100

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,54 @@
+import string
+
+def encrypt(text):
+    alphabet = list(string.ascii_uppercase)
+    text = text.upper()
+    newText = ""
+    for i in text:
+        if i in alphabet:
+            order = alphabet.index(i)
+            if order + 6 > 26:
+                overflow = order + 6 - 26
+                newText += alphabet[overflow]
+            else:
+                newText += alphabet[order+6]
+        else:
+            try:
+                i = int(i)
+                if i + 6 > 9:
+                    overflow = i + 6 - 9
+                    newText += overflow
+                else:
+                    newText += str(i + 6)
+            except:
+                newText += str(i)
+            
+    return newText
+
+def decrypt(text):
+    alphabet = list(string.ascii_uppercase)
+    text = text.upper()
+    newText = ""
+    for i in text:
+        if i in alphabet:
+            order = alphabet.index(i)
+            if order - 6 < 0:
+                order = str(order-6)
+                overflow = order[1:]
+                newText += alphabet[26 - int(overflow)]
+            else:
+                newText += alphabet[order-6]
+        else:
+            try:
+                i = int(i)
+                if i - 6 < 0:
+                    overflow = i - 6
+                    overflow = str(overflow[1:])
+                    newText += overflow
+                else:
+                    newText += str(i - 6)
+            except:
+                newText += str(i)
+            
+    return newText
+
